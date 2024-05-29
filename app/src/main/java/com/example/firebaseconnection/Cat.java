3f0041c0db4fb4e15f8147f83c3bb3e907db103a
Simon Escano <escanosimonlyster@gmail.com>
2024-05-29 10:24:28 +0800

    did generate layout

---
+++ app/src/main/java/com/example/firebaseconnection/Cat.java
@@ -0,0 +1,21 @@
+package com.example.firebaseconnection;
+
+import android.widget.LinearLayout;
+
+public class Cat {
+
+    public String catImageURL;
+    public String catName;
+    public int catPrice;
+    public int catRarity;
+
+    public Cat(String catImageURL, String catName, int catPrice, int catRarity) {
+
+    }
+
+    public LinearLayout generate() {
+        LinearLayout linearLayout;
+
+        return linearLayout;
+    }
+}
