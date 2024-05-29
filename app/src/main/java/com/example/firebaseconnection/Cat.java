defb31f2836e612655a28d9fc9bad757d7c64af7
Simon Escano <escanosimonlyster@gmail.com>
2024-05-29 10:09:46 +0800

    did generate layout

--- app/src/main/java/com/example/firebaseconnection/Cat.java
+++ app/src/main/java/com/example/firebaseconnection/Cat.java
@@ -1,12 +1,14 @@
-//package com.example.firebaseconnection;
-//
-//import android.widget.LinearLayout;
-//
-//public class Cat extends LinearLayout {
-//
-//
-//
-////    public Cat() {
-////
-////    }
-//}
+package com.example.firebaseconnection;
+
+import android.widget.LinearLayout;
+
+public class Cat {
+    public Cat() {
+
+    }
+
+    public LinearLayout generate() {
+        LinearLayout linearLayout;
+        return linearLayout;
+    }
+}
